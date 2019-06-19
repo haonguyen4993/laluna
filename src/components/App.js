a731bca89f9c4c49e871ce72c2e1a18d9bcdd7eb
haonguyen4993 <nguyen.thi.hao@framgia.com>
2019-06-19 16:52:33 +0700

    add styled-components

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.css";
+
+import React from "react";
+
+const App = () => {
+	return (
+		<div className="container">Hello</div>
+	);
+};
+
+export default App;
---
+++ src/components/common/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+	display: inline-block;
+	padding: 0.5rem 1rem;
+	border: 1px solid black;
+	color: black;
+	background-color: white;
+`;
+
+export default Button
